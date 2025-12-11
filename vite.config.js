2ae1fea0b704859aab0d6ea487cf7fa1254e5f3d
Helen <lin.helen517@gmail.com>
2025-12-11 15:47:06 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,7 @@ export default defineConfig({
       // https://vitejs.dev/guide/build.html#multi-page-app
       input: {
         main: resolve(__dirname, "src/index.html"),
-        primitives: resolve(__dirname, "src/primitives/index.html"),
-        materials: resolve(__dirname, "src/materials/index.html"),
-        textures: resolve(__dirname, "src/textures/index.html"),
-        groups: resolve(__dirname, "src/groups/index.html"),
-        exercise: resolve(__dirname, "src/exercise/index.html"),
+        gallery: resolve(__dirname, "src/gallery/index.html"),
       },
     },
   },
